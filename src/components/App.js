a6fd6668829a14c9eddd36e0534cf779ed549700
lebohang0824 <lebohang0824@gmail.com>
2020-02-13 15:19:06 +0200

    Server data test

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import axios from 'axios';
 import './App.scss';
 
-function App() {
-  return (<h1>ReaJs</h1>);
+class App extends React.Component {
+
+	constructor(props) {
+		super(props)
+
+		this.state = { welcome: ''}
+	}
+
+	componentDidMount() {
+		axios.get('/api/message').then(res => {
+			this.setState({ welcome: res.data });
+		});
+	}
+
+
+	render() {
+		return this.state.welcome; 
+	}
+
 }
 
 export default App;
